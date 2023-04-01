6f7fd7396f1980d8e089e56f8da85f19537439b1
bashdef <themario717@gmail.com>
2023-04-01 04:53:24 +0300

    complete

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bashdef/yp_server',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '93e4363cb092b5d9fc1eb03338f0532b2dafbc6b',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bashdef/yp_server' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '93e4363cb092b5d9fc1eb03338f0532b2dafbc6b',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
