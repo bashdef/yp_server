c3f6edbada208f908e86154b0061405718e9dd3e
bashdef <themario717@gmail.com>
2023-04-04 08:46:28 +0300

    ф-ия аутентификации

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -4,6 +4,7 @@ use Model\Post;
 use Src\View;
 use Src\Request;
 use Model\User;
+use Src\Auth\Auth;
 class Site
 {
     public function index(Request $request): string
@@ -22,5 +23,22 @@ class Site
             app()->route->redirect('/go');
         }
         return new View('site.signup');
+    }
+
+    public function login(Request $request): string
+    {
+        if($request->method === 'GET'){
+            return new View('site.login');
+        }
+        if(Auth::attempt($request->all())){
+            app()->route->redirect('/hello');
+        }
+
+        return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+    }
+    public function logout(): void
+    {
+        Auth::logout();
+        app()->route->redirect('/hello');
     }
 }
